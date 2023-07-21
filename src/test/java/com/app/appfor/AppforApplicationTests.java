c76a359c8c2b746ab26a0c16e48e6f313602d325
FirasBenNacib <128628709+firassBenNacib@users.noreply.github.com>
2023-07-21 15:35:24 +0100

    added jacoco

--- src/test/java/com/app/appfor/AppforApplicationTests.java
+++ src/test/java/com/app/appfor/AppforApplicationTests.java
@@ -1,13 +1,43 @@
 package com.app.appfor;
 
+import com.app.appfor.controller.MetricsController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AppforApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private MetricsController metricsController;
+
+	@BeforeEach
+	void setUp() {
+		MeterRegistry meterRegistry = new SimpleMeterRegistry();
+		metricsController = new MetricsController(meterRegistry);
 	}
 
+	@Test
+	void trackTraffic_shouldIncrementTrafficCount() {
+		metricsController.trackTraffic();
+		double trafficCount = metricsController.getTrafficCount();
+		assertEquals(1, trafficCount);
+	}
+
+	@Test
+	void decrementTraffic_shouldDecrementTrafficCount() {
+		metricsController.trackTraffic();
+		metricsController.decrementTrafficmanual();
+		double trafficCount = metricsController.getTrafficCount();
+		assertEquals(0, trafficCount);
+	}
+
+	// Add more test methods to cover other functionality of MetricsController
 }
