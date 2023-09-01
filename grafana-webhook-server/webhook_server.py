3b2b2b7cfc273d3f508206e560395cbd60e6db44
FirasBenNacib <128628709+firassBenNacib@users.noreply.github.com>
2023-09-01 13:16:56 +0100

    update

--- grafana-webhook-server/webhook_server.py
+++ grafana-webhook-server/webhook_server.py
@@ -15,15 +15,25 @@ def grafana_webhook():
         return jsonify({"message": "No data received"}), 400
 
     state = data.get("state", "")
-    
-    if state == "alerting":
-        queue_size = get_queue_size_from_prometheus()
-        n = (queue_size // 1000)
+    queue_difference = get_queue_difference_from_prometheus()
+    queue_size = get_queue_size_from_prometheus()
 
-        desired_target_queue_size = int(queue_size // (n-1))
+
+    if queue_size == 0:
+        return jsonify({"message": "Queue size is 0. No action taken."}), 200
+    
+    if state == "alerting" and queue_difference < 200:
+       
+        n = (queue_size // 1000)
+        
+        if n == 1:
+            desired_target_queue_size = 10000
+        else:
+            desired_target_queue_size = int((queue_size // (n-1)) - 100)
+     
         print(f"New Queue Size: {desired_target_queue_size}")
         modify_scaled_object_target_queue_size(str(desired_target_queue_size))
-    elif state == "ok":
+    elif state == "ok" and queue_difference > 200:
         modify_scaled_object_target_queue_size("1000")
         
     return jsonify({"message": "Webhook received"}), 200
@@ -42,6 +52,23 @@ def get_queue_size_from_prometheus():
     print(f"Extracted Queue Size: {queue_size}")
 
     return queue_size
+    
+def get_queue_difference_from_prometheus():
+    prometheus_url = "http://10.110.96.5:9090/api/v1/query"   # NOSONAR
+    query = {"query": "queue_difference_metric"}  
+    response = requests.get(prometheus_url, params=query)
+    result = response.json()
+
+    print(f"Prometheus Response for Queue Difference: {result}")
+
+    try:
+        queue_difference = int(float(result['data']['result'][0]['value'][1]))
+    except (KeyError, IndexError, TypeError, ValueError):
+        print("No data in queue difference")
+        return 0
+
+    return queue_difference
+
 
 def modify_scaled_object_target_queue_size(new_size):
     api_instance = client.CustomObjectsApi()
